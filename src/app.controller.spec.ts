2d3c6327150cd12c86b537b6e8d909993098c7ce
haikelz <haikel.2017th@gmail.com>
2023-09-18 20:10:43 +0700

    test: replace `ts-jest` with `swc-jest`

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe("AppController", () => {
   });
 
   describe("root", () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello!"', () => {
       expect(appController.getHello()).toBeDefined();
     });
   });
